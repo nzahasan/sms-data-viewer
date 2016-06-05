198cb7957556abb731bc48e1f8e6f5747c8ef80f
Kraken <nzahasan@gmail.com>
2016-06-05 22:35:15 +0600

    fixed not loding

--- include/connect.php
+++ include/connect.php
@@ -6,10 +6,6 @@ $dbname = "sms_dbase";
 
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-if(!$connection){
-	header('Location: ../error/')
-}
-
 //setting charecterset to utf8 unicode for supporting BANGLA AND OTHER//
 mysqli_query($connection,'SET CHARACTER SET utf8');
 
